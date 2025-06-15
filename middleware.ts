075bafe5e9db66f6ff1bcf0bfdefd69dbbe4c5d0
ghost_designer <congotech17@gmail.com>
2025-06-16 01:37:18 +0200

    Correction bug sur affichage budget et creation

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
@@ -8,29 +7,12 @@ export default clerkMiddleware(async (auth, req) => {
     await auth.protect();
   }
 });
-=======
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forum(.*)'])
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
-})
->>>>>>> a4eda5d1dede6b138bee1c37b53187c445dc704a
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-<<<<<<< HEAD
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
-=======
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
->>>>>>> a4eda5d1dede6b138bee1c37b53187c445dc704a
